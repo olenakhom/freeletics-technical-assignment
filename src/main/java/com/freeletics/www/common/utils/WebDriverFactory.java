2ce4991883b64c69ec65eb3d6f74597387df6554
Olena Khomenko <okhomenko@rubiconproject.com>
2019-02-16 13:02:05 +0200

    Added setting for initialization of Android web driver

--- src/main/java/com/freeletics/www/common/utils/WebDriverFactory.java
+++ src/main/java/com/freeletics/www/common/utils/WebDriverFactory.java
@@ -5,20 +5,33 @@ import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Value;
 
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 public class WebDriverFactory {
+
+    @Value("${mobile.os.version}")
+    private String mobileOsVersion;
 
     public WebDriver getDriver(String runEnv, String rDriverUrl) throws MalformedURLException {
         if ("local".equals(runEnv)) {
             setupChromeDriverManager();
             return new ChromeDriver();
-        } else if ("remote".equals(runEnv)) {
-            return new RemoteWebDriver(new URL(rDriverUrl), getChromeDesiredCapabilities());
-        } else {
+        }
+        else if ("remote".equals(runEnv)) {
+            return new RemoteWebDriver(new URL(rDriverUrl), getChromeCapabilities());
+        }
+        else if ("mobile".equals(runEnv)) {
+            return new AndroidDriver(new URL(rDriverUrl), getAndroidChromeCapabilities());
+        }
+        else {
             throw new IllegalArgumentException(String.format("No such run enviroment - '%s'.", runEnv));
         }
     }
@@ -28,12 +41,20 @@ public class WebDriverFactory {
         chromeDriverManager.setup();
     }
 
-    private DesiredCapabilities getChromeDesiredCapabilities() {
-        DesiredCapabilities capability = DesiredCapabilities.chrome();
-        capability.setBrowserName("chrome");
-        capability.setCapability("enableVNC", true);
-        capability.setCapability("enableVideo", false);
-        return capability;
+    private DesiredCapabilities getChromeCapabilities() {
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setBrowserName("chrome");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+        return capabilities;
+    }
+
+    private DesiredCapabilities getAndroidChromeCapabilities(){
+        DesiredCapabilities capabilities = DesiredCapabilities.android();
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,BrowserType.CHROME);
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"My Android");
+        capabilities.setCapability(MobileCapabilityType.VERSION, mobileOsVersion);
+        return capabilities;
     }
 
 }
